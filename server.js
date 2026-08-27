ac6cf5e7e7515932bc36795ba31d8f30a03e9b53
Samandar(Sam) Turdaliev <abc@abcs-MacBook-Air.local>
2026-08-27 15:30:05 -0700

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+})
 
 // 1: Kirish codlari
 app.use(express.static("public"));
@@ -21,6 +31,9 @@ app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({ test: "success" });
 })
+app.get("/author", function (req, res) {
+    res.render("author", {user: user});
+});
 app.get("/", function (req, res) {
     res.render("harid");
 });
