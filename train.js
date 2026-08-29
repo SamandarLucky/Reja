8a3ca5fb1c20253d5d4e954df5a93b862501a283
Samandar(Sam) Turdaliev <abc@abcs-MacBook-Air.local>
2026-08-29 09:32:23 -0700

    feat: MITASK A completed

--- train.js
+++ train.js
@@ -1,60 +1,22 @@
 //==================================================================================================================
 
+                                                    // TASK A
 
+function countLetter(harf, soz) {
+    let count = 0;
+
+    for (let i = 0; i < soz.length; i++) {
+        if (soz[i] === harf) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countLetter("e", "TurdalievSamandar")); 
 //==================================================================================================================
 
-// Asynchronous functionlarni qo'llash
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba boling",
-    "togri boshliq tanlang va koproq hato qiling",
-    "uzingizga ishlashni boshlang",
-    "siz kuchli bolgan narsalarni qiling",
-    "yoshlarga invest qiling",
-    "endi dam oling, foydasi yoq endi",
-
-];
-
-async function maslahatBering(a) {
-    if (typeof a !== 'number') throw new Error("insert number");
-    else if(a <= 20) return list[0];
-    else if(a > 20 && a <= 30) return list[1];
-    else if(a > 30 && a <= 40) return list[2];
-    else if(a > 40 && a <= 50) return list[3];
-    else if(a > 50 && a <= 60) return list[4];
-    else {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(list[5]);
-            }, 5000);
-        });
-}}
-
-// THEN & CATCH
-
-// console.log('passed here 0')
-// maslahatBering(25).then(data => { // then & catch
-//     console.log('JAVOB:', data);
-// }).catch(err => {
-// console.log('ERROR:', err);
-// })
-// console.log("passed here 1")
-
-// ASYN & WAIT
-
-async function run() {
-    let javob = await maslahatBering(65);
-    console.log(javob);
-    javob = await maslahatBering(31);
-    console.log(javob);
-    javob = await maslahatBering(41);
-    console.log(javob);
-}
-run();
-
-//=================================================================================================================
-
-// EVENT LOOP va CALLBACK FUNCTIONLAR
+                                                    // Asynchronous functionlarni qo'llash
 
 // console.log("Jack Ma maslahatlari");
 // const list = [
@@ -66,6 +28,47 @@ run();
 //     "endi dam oling, foydasi yoq endi",
 
 // ];
+
+// async function maslahatBering(a) {
+//     if (typeof a !== 'number') throw new Error("insert number");
+//     else if(a <= 20) return list[0];
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(list[5]);
+//             }, 5000);
+//         });
+// }}
+
+                                                    // THEN & CATCH
+
+// console.log('passed here 0')
+// maslahatBering(25).then(data => { // then & catch
+//     console.log('JAVOB:', data);
+// }).catch(err => {
+// console.log('ERROR:', err);
+// })
+// console.log("passed here 1")
+
+                                                    // ASYN & WAIT
+
+// async function run() {
+//     let javob = await maslahatBering(65);
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
+
+//=================================================================================================================
+
+                                                // EVENT LOOP va CALLBACK FUNCTIONLAR
 
 // function maslahatBering(a, callback) {
 //     if(typeof a !== 'number') callback("insert number", null);
