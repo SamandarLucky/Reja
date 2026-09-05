506d8bbdb0ca1a62ca355e77881059e81cefc033
Samandar(Sam) Turdaliev <abc@abcs-MacBook-Air.local>
2026-09-05 07:26:42 -0700

    feat: FrontEnd JS qurish

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
-// const res = require("express/lib/response");
 const app = express();
 
 
 // MongoDB connect
 const db = require("./server").db();
+const mongodb = require("mongodb");
 // USER JSON
 const user = require("./database/user.json");
 
@@ -23,23 +23,28 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
-    console.log('user entered / create item');
-    console.log(req.body);
+    console.log("user entered / create-item");
     const new_reja = req.body.reja;
-    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end('something went wrong');
-        } else {
-            res.end('successfully added');
-        }
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+      res.json(data.ops[0]);
     });
-})
+});
+
+app.post("/delete-item", (req, res) => {
+ const id = req.body.id;
+ db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function(err, data) {
+    res.json({state: "success"});
+ });
+ 
+});
+
+
 app.get("/author", function (req, res) {
     res.render("author", {user: user});
 });
+
 app.get("/", function (req, res) {
-    console.log('user entered /');
+    console.log("user entered /");
     db.collection("plans").find().toArray((err, data) => {
         if(err) {
             console.log(err);
